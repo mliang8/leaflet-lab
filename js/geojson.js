30a99371602f423d4462a3e1c711b37aedc7b4e5
mliang8 <Amber@Mengyus-MacBook-Air.local>
2017-02-13 23:47:51 -0600

    module 4 assignment progress

--- js/geojson.js
+++ js/geojson.js
@@ -3,34 +3,62 @@
 
 /*initialize a map with a set of view extent and zoom levels*/
 
-
-
-// add a tileset from leaftlet provider and then add to mymap
-
-
-//load the dat from MegaCities.geojson using ajax
-
-
-
-
-//function to instantiate the Leaflet map
-/* Map of GeoJSON data from MegaCities.geojson */
-
-//function to instantiate the Leaflet map
 function createMap(){
-    //create the map
-    var map = L.map('map', {
-        center: [45, -50],
-        zoom: 2
-    });
+    var map=L.map('map',{
+    center:[20,0],
+    zoom:2
+});
 
-    //add OSM base tilelayer
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+        //maxZoom: 18,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    //call getData function
     getData(map);
+};
+
+function getData(map){
+    $.ajax('data/MegaCities.geojson',{
+        dataType:'json',
+        success: function (response){
+            L.geoJson(response).addTo(map);
+        }
+    });
+};
+
+$(document).ready(createMap);
+
+$.ajax("data/MegaCities.geojson", {
+        dataType: "json",
+        success: function(response){
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response, {
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                }
+            }).addTo(map);
+        }
+    });
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
 };
 
 //function to retrieve the data and place it on the map
@@ -41,11 +69,24 @@ function getData(map){
         success: function(response){
 
             //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response).addTo(map);
-            //console.log('yes');
+            L.geoJson(response, {
+                onEachFeature: onEachFeature
+            }).addTo(map);
         }
     });
 };
 
-$(document).ready(createMap);
+ $.ajax("data/MegaCities.geojson", {
+        dataType: "json",
+        success: function(response){
+
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response, {
+                //use filter function to only show cities with 2015 populations greater than 20 million
+                filter: function(feature, layer) {
+                    return feature.properties.Pop_2015 > 20;
+                }
+            }).addTo(map);
+        }
+    });
 
