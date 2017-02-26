65f71070658476dd0a88f9c150e8ce4b1caaa791
mliang8 <Amber@Mengyus-MacBook-Air.local>
2017-02-25 21:52:17 -0600

    update on data and main.js

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ function onEachFeature(feature, layer){
 
 
 //create a getData function using the map as a parametr to add a stylized marker and the attribute information onto the map
-function getData(mymap){
+/*function getData(mymap){
 	//using ajax to load the geojson file
 	$.ajax('data/cities_pop.geojson',{
 		//specify the file type
@@ -60,9 +60,87 @@ function getData(mymap){
 			/*L.geoJson(response, {
                 onEachFeature: onEachFeature
             }).addTo(mymap);*/
+/*		}
+	});
+};*/
+
+//create a function for adding the proportional symbols on the map with dataset and the map itself as paarameters
+function createPropSymbols(data,mymap){
+	//define a variable for marker stylizing
+	var geojsonMarkerOptions={
+		radius: 10,
+		fillColor:"#85C1E9",
+		color:"#3498DB",
+		weight:2,
+		opacity:1,
+		fillOpacity:0.85
+	};
+	//define a variable for the attribute to visualize with the proportional symbols on map, which is the population of 1970 in thousand 
+	var attribute= "pop_1970(thousands)";
+	//create a geojson layer on map and add to map
+	L.geoJson(data,{
+		//internally call the pointToLayer function to spawn the data points' lat and lon values to the geojson layer created
+		pointToLayer: function (feature, latlng){
+			//define a variable to store the attribute values which is 1970 population of each city and cast the value to a numeric value
+			var attValue=Number(feature.properties[attribute]);
+			//console.log(feature.properties, attValue);
+			//acess the radius of each marker defined previously and change it based on pop size by calling the calcPropRadius function 
+				//with the attribute value, whcih is the 1970 pop size
+			geojsonMarkerOptions.radius=calcPropRadius(attValue);
+			//create the stylized circle markers at each city's locations 
+			return L.circleMarker(latlng, geojsonMarkerOptions);
+		}
+	}).addTo(mymap);
+};
+//create a function to import data from the geojson file
+function getData(mymap){
+	//using ajax to load in the data  
+	$.ajax("data/cities_pop.geojson",{
+		dataType:"json",
+		//call the function to create proportional symbol marker when the data is loaded in
+		success:function (response){
+			createPropSymbols(response, mymap);
 		}
 	});
 };
+//create a function to calculate the radius of markers based on attValue which is the population size
+function calcPropRadius(attValue){
+	//define a variable scale factor to scale down all the marker symbols based on their numerical values
+	var scaleFactor=0.5;
+	//calculate the marker size based on the attribute values and the scale factor which adjusts the values
+	var area=attValue * scaleFactor;
+	//calculate the radius of circle marker based on basic equation for calculating circle area
+	var radius= Math.sqrt(area/Math.PI);
+	//return te radius of symbol
+	return radius;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //call the createMap function when the document has loaded
